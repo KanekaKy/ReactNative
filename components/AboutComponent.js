ce8fb9b7d97a340137820e781ee49bdcfc22925a
Kaneka <kaneka.ky@gmail.com>
2020-01-04 12:12:35 -0800

    Task2

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
-class About {
+
+class About extends Component {
     static navigationOptions = {
         title: 'About Us'
     }
     render() {
-        return (<ScrollView> </ScrollView>);
+        return (<ScrollView></ScrollView>);
     }
 }
 export default About;
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
+import { Card, Text } from 'react-native-elements'
 
-class Contact {
+class Contact extends Component {
     static navigationOptions = {
         title: 'Contact Us'
     }
     render() {
-        return (<ScrollView> </ScrollView>);
+        return (<ScrollView>
+            <Card wrapperStyle={{ margin: 20 }}
+                title="Contact Information">
+                <Text Style= {{marginBottom: 10}}>
+                    1 Nucamp Way {"\n"}
+                    Seattle, WA 98001 {"\n"}
+                    U.S.A. {"\n"}</Text>
+                <Text>Phone:1234-5678 {"\n"}
+                    Email:Campsite@info.co </Text>
+            </Card>
+        </ScrollView>);
     }
 }
 export default Contact;
