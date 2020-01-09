75a4aad558b8e02b57c6e57333f46c12e5dd2f16
Kaneka <kaneka.ky@gmail.com>
2020-01-08 22:42:51 -0800

    Icons, Favorites, and Comments

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList  } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Text, View, ScrollView, FlatList } from 'react-native';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
+import { Card, Icon } from 'react-native-elements';
 
-function RenderCampsite({campsite}) {
-   
+function RenderCampsite(props) {
+    const { campsite } = props;
     if (campsite) {
         return (
             <Card
@@ -14,41 +14,54 @@ function RenderCampsite({campsite}) {
                 <Text style={{margin: 10}}>
                     {campsite.description}
                 </Text>
+                <Icon
+                    name={props.favorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    raised
+                    reverse
+                    onPress={() => props.favorite ? 
+                        console.log('Already set as a favorite') : props.markFavorite()}
+                />
             </Card>
         );
     }
     return <View />;
 }
 
-function RenderComments({comments}){
-    const renderCommentItem = ({item}) => {
+function RenderComments({ comments }) {
+    const renderCommentItem = ({ item }) => {
         return (
-            <View style={{margin: 10}}>
-                <Text style={{fontSize: 14}}>{item.text}</Text>
-                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
-                <Text style={{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
+            <View style={{ margin: 10 }}>
+                <Text style={{ fontSize: 14 }}>{item.text}</Text>
+                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Text style={{ fontSize: 12 }}>{`-- ${item.author}, ${item.date}`}</Text>
             </View>
         );
     };
 
-    return(
+    return (
         <Card title='Comment'>
-            <FlatList 
-            data={comments}
-            renderItem={renderCommentItem}
-            keyExtractor= {item =>item.id.toString()}
+            <FlatList
+                data={comments}
+                renderItem={renderCommentItem}
+                keyExtractor={item => item.id.toString()}
             />
         </Card>
     )
 }
 class CampsiteInfo extends Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            comments: COMMENTS
+            comments: COMMENTS,
+            favorite: false
         };
+    }
+    markFavorite() {
+        this.setState({favorite: true});
     }
 
     static navigationOptions = {
@@ -61,7 +74,10 @@ class CampsiteInfo extends Component {
         const comments = this.state.comments.filter(comment => comment.campsiteId === campsiteId);
         return (
             <ScrollView>
-                <RenderCampsite campsite={campsite} />
+                <RenderCampsite campsite={campsite}
+                    favorite={this.state.favorite}
+                    markFavorite={() => this.markFavorite()}
+                />
                 <RenderComments comments={comments} />
             </ScrollView>
         );
